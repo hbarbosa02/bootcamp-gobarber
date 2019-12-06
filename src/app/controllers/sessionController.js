3ff4a762587941ddc50a65aa871c3be1ee38a30e
Hiran Gondim <hiran.g.b.silva@gmail.com>
2019-12-06 09:58:14 -0300

    Ajustes para o melhor funcionamento do front.

--- src/app/controllers/sessionController.js
+++ src/app/controllers/sessionController.js
@@ -3,6 +3,7 @@ import * as Yup from 'yup';
 import authConf from '../../config/auth';
 
 import User from '../models/User';
+import File from '../models/File';
 
 class SessionController {
   async store(req, res) {
@@ -18,20 +19,27 @@ class SessionController {
 
     const { email, password } = req.body;
 
-    const user = await User.findOne({ where: { email: req.body.email } });
+    const user = await User.findOne({
+      where: { email: req.body.email },
+      include: [
+        { model: File, as: 'avatar', attributes: ['id', 'path', 'url'] },
+      ],
+    });
 
     if (!user) return res.status(401).json({ error: 'User not found' });
 
     if (!(await user.checkPassword(password)))
       return res.status(401).json({ error: 'Password does not match' });
 
-    const { id, name } = user;
+    const { id, name, avatar, provider } = user;
 
     return res.json({
       user: {
         id,
         name,
         email,
+        avatar,
+        provider,
       },
       token: jwt.sign({ id }, authConf.jwtKey),
     });
